28cb531503064a8957843c047e13e51b9ee2286b
bel1c10ud <mail@bellclou.de>
2025-10-18 17:54:23 +0900

    feat: 커스텀 구분자 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,12 +8,31 @@ class App {
       );
       return input;
     } catch (error) {
-
+      
     }
   }
   splitInput(input) {
-    const DEFAULT_SEPARATOR_REGEX = new RegExp(/,|:/);
-    const numbers = input.split(DEFAULT_SEPARATOR_REGEX).map(Number);
+    const separators = [",", ":"];
+    const CUSTOM_SEPARATOR_REGEX = new RegExp(/^\/\/(\D)\\n/);
+
+    // 커스텀 구분자를 지정하였는지 확인
+    if (CUSTOM_SEPARATOR_REGEX.test(input)) {
+      const customSeparator = input
+        .match(CUSTOM_SEPARATOR_REGEX)[1]
+        // 정규표현식에서 사용하는 특수문자 이스케이핑
+        .replace(/[.*+?^${}()|[\]\\]/, "\\$&");
+
+      // 구분자 추가
+      separators.push(customSeparator);
+    }
+
+    const SEPARATOR_REGEX = new RegExp(`${separators.join("|")}`);
+
+    const numbers = input
+      .replace(CUSTOM_SEPARATOR_REGEX, "")
+      .split(SEPARATOR_REGEX)
+      .map(Number);
+
     return numbers;
   }
   async run() {
@@ -21,7 +40,7 @@ class App {
     const numbers = this.splitInput(input);
     const result = numbers.reduce((prev, cur) => prev + cur, 0);
 
-    Console.print(`결과 : ${result}`);  
+    Console.print(`결과 : ${result}`);
   }
 }
 
