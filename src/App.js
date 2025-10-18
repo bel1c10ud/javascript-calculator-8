73a5043394d00db2b7e754c144f0310408290463
bel1c10ud <mail@bellclou.de>
2025-10-18 16:11:03 +0900

    feat: 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async getInput() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      return input;
+    } catch (error) {
+
+    }
+  }
+  async run() {
+    const input = await this.getInput();
+
+    Console.print(`입력: ${input}`);
+  }
 }
 
 export default App;
